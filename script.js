b8cf0f3077048c9394b1a0f1708a537522c2af03
Roz Barnes <rozbarnes@gmail.com>
2020-06-14 23:12:11 +0000

    added number fields in and put semi colons in.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let form = document.querySelector("#parking-form");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let name = document.querySelector("#name")
-    let nameField = document.querySelector("#name-field")
+    let name = document.querySelector("#name");
+    let nameField = document.querySelector("#name-field");
     console.log(nameField.classList);
     if (name.value === '' || name.value == null) {
-        alert("name is required")
-        nameField.classList.add("input-invalid")
+        alert("name is required");
+        nameField.classList.add("input-invalid");
     }
     else {
-        nameField.classList.remove("input-invalid")
-        nameField.classList.add("input-valid")
+        nameField.classList.remove("input-invalid");
+        nameField.classList.add("input-valid");
     }
 
     console.log(nameField.value);
@@ -18,17 +18,16 @@ form.addEventListener("submit", function (event) {
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let carField = document.querySelector("#car-field")
-    let carYear = document.querySelector("#car-year")
+    let carField = document.querySelector("#car-field");
+    let carYear = document.querySelector("#car-year");
     console.log(carYear.classList);
     if (carYear.value === '' || carYear.value == null) {
-        alert("car year is required")
-        carField.classList.add("input-invalid")
-        if (carYear.value > 1900);
+        alert("car year is required");
+        carField.classList.add("input-invalid");
     }
     else {
-        carField.classList.remove("input-invalid")
-        carField.classList.add("input-valid")
+        carField.classList.remove("input-invalid");
+        carField.classList.add("input-valid");
     }
     //(document.querySelector("input-field")
     // if (document.getElementById('name')value)
@@ -38,16 +37,16 @@ form.addEventListener("submit", function (event) {
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let carField = document.querySelector("#car-field")
-    let carMake = document.querySelector("#car-make")
+    let carField = document.querySelector("#car-field");
+    let carMake = document.querySelector("#car-make");
     console.log(carMake.classList);
     if (carMake.value === '' || carMake.value == null) {
-        alert("car make is required")
-        carField.classList.add("input-invalid")
+        alert("car make is required");
+        carField.classList.add("input-invalid");
     }
     else {
-        carField.classList.remove("input-invalid")
-        carField.classList.add("input-valid")
+        carField.classList.remove("input-invalid");
+        carField.classList.add("input-valid");
     }
 
     console.log(carYear.value);
@@ -55,63 +54,50 @@ form.addEventListener("submit", function (event) {
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let carField = document.querySelector("#car-field")
-    let carModel = document.querySelector("#car-model")
+    let carField = document.querySelector("#car-field");
+    let carModel = document.querySelector("#car-model");
     console.log(carModel.classList);
     if (carModel.value === '' || carModel.value == null) {
-        alert("car model is required")
-        carField.classList.add("input-invalid")
+        alert("car model is required");
+        carField.classList.add("input-invalid");
     }
     else {
-        carField.classList.remove("input-invalid")
-        carField.classList.add("input-valid")
+        carField.classList.remove("input-invalid");
+        carField.classList.add("input-valid");
     }
     console.log(carYear.value);
 })
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let startDateField = document.querySelector("#start-date-field")
-    let startDate = document.querySelector("#start-date")
+    let startDateField = document.querySelector("#start-date-field");
+    let startDate = document.querySelector("#start-date");
     console.log(startDate.classList);
     if (startDate.value === '' || startDate.value == null) {
-        alert("start date is required")
-        startDateField.classList.add("input-invalid")
+        alert("start date is required");
+        startDateField.classList.add("input-invalid");
     }
     else {
-        startDateField.classList.remove("input-invalid")
-        startDateField.classList.add("input-valid")
+        startDateField.classList.remove("input-invalid");
+        startDateField.classList.add("input-valid");
     }
-    //(document.querySelector("input-field")
-    // if (document.getElementById('name')value)
-    //alert('field is required');
-
-    //alert("field is required");
-
-
 
     console.log(carYear.value);
 })
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let daysField = document.querySelector("#days-field")
-    let days = document.querySelector("#days")
+    let daysField = document.querySelector("#days-field");
+    let days = document.querySelector("#days");
     console.log(days.classList);
     if (days.value === '' || days.value == null) {
-        alert("Number of days are required")
-        daysField.classList.add("input-invalid")
+        alert("Number of days are required");
+        daysField.classList.add("input-invalid");
     }
     else {
-        daysField.classList.remove("input-invalid")
-        daysField.classList.add("input-valid")
+        daysField.classList.remove("input-invalid");
+        daysField.classList.add("input-valid");
     }
-    //(document.querySelector("input-field")
-    // if (document.getElementById('name')value)
-    //alert('field is required');
-
-    //alert("field is required");
-
 
 
     console.log(daysField.value);
@@ -119,23 +105,17 @@ form.addEventListener("submit", function (event) {
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let creditCardField = document.querySelector("#credit-card-field")
-    let creditCard = document.querySelector("#credit-card")
+    let creditCardField = document.querySelector("#credit-card-field");
+    let creditCard = document.querySelector("#credit-card");
     console.log(creditCard.classList);
     if (creditCard.value === '' || creditCard.value == null) {
-        alert("Credit card number required")
-        creditCardField.classList.add("input-invalid")
+        alert("Credit card number required");
+        creditCardField.classList.add("input-invalid");
     }
     else {
-        creditCardField.classList.remove("input-invalid")
-        creditCardField.classList.add("input-valid")
+        creditCardField.classList.remove("input-invalid");
+        creditCardField.classList.add("input-valid");
     }
-    //(document.querySelector("input-field")
-    // if (document.getElementById('name')value)
-    //alert('field is required');
-
-    //alert("field is required");
-
 
 
     console.log(daysField.value);
@@ -143,23 +123,17 @@ form.addEventListener("submit", function (event) {
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let cvvField = document.querySelector("#cvv-field")
-    let cvv = document.querySelector("#cvv")
+    let cvvField = document.querySelector("#cvv-field");
+    let cvv = document.querySelector("#cvv");
     console.log(cvv.classList);
     if (cvv.value === '' || cvv.value == null) {
-        alert("CVV is required")
-        cvvField.classList.add("input-invalid")
+        alert("CVV is required");
+        cvvField.classList.add("input-invalid");
     }
     else {
-        cvvField.classList.remove("input-invalid")
-        cvvField.classList.add("input-valid")
+        cvvField.classList.remove("input-invalid");
+        cvvField.classList.add("input-valid");
     }
-    //(document.querySelector("input-field")
-    // if (document.getElementById('name')value)
-    //alert('field is required');
-
-    //alert("field is required");
-
 
 
     console.log(daysField.value);
@@ -167,24 +141,17 @@ form.addEventListener("submit", function (event) {
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let expirationField = document.querySelector("#expiration-field")
-    let expiration = document.querySelector("#expiration")
+    let expirationField = document.querySelector("#expiration-field");
+    let expiration = document.querySelector("#expiration");
     console.log(expiration.classList);
     if (expiration.value === '' || expiration.value == null) {
-        alert("Expiration date is required")
-        expirationField.classList.add("input-invalid")
+        alert("Expiration date is required");
+        expirationField.classList.add("input-invalid");
     }
     else {
-        expirationField.classList.remove("input-invalid")
-        expirationField.classList.add("input-valid")
+        expirationField.classList.remove("input-invalid");
+        expirationField.classList.add("input-valid");
     }
-    //(document.querySelector("input-field")
-    // if (document.getElementById('name')value)
-    //alert('field is required');
-
-    //alert("field is required");
-
-
 
     console.log(daysField.value);
 })
